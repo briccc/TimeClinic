0d01243790ba306233f14e14586ed6dcb64220b0
Luciana Belén Canteros <lucianacanteros2010@hotmail.com>
2023-09-24 20:26:46 -0300

    Agregar nuevo turno vistas

--- TimeClinic/Recepcionista/RecepcionistaControl/UC_NuevoTurno.Designer.cs
+++ TimeClinic/Recepcionista/RecepcionistaControl/UC_NuevoTurno.Designer.cs
@@ -28,10 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.BAgregarNuevoTurno = new System.Windows.Forms.Button();
+            this.TBPaciente = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DTDiaTurno = new System.Windows.Forms.DateTimePicker();
+            this.CBPaciente = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // BAgregarNuevoTurno
+            // 
+            this.BAgregarNuevoTurno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(104)))), ((int)(((byte)(91)))));
+            this.BAgregarNuevoTurno.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(104)))), ((int)(((byte)(91)))));
+            this.BAgregarNuevoTurno.FlatAppearance.BorderSize = 2;
+            this.BAgregarNuevoTurno.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(104)))), ((int)(((byte)(91)))));
+            this.BAgregarNuevoTurno.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(104)))), ((int)(((byte)(91)))));
+            this.BAgregarNuevoTurno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BAgregarNuevoTurno.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BAgregarNuevoTurno.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.BAgregarNuevoTurno.Location = new System.Drawing.Point(543, 430);
+            this.BAgregarNuevoTurno.Margin = new System.Windows.Forms.Padding(4);
+            this.BAgregarNuevoTurno.Name = "BAgregarNuevoTurno";
+            this.BAgregarNuevoTurno.Size = new System.Drawing.Size(136, 42);
+            this.BAgregarNuevoTurno.TabIndex = 29;
+            this.BAgregarNuevoTurno.Text = "Agregar";
+            this.BAgregarNuevoTurno.UseVisualStyleBackColor = false;
+            // 
+            // TBPaciente
+            // 
+            this.TBPaciente.Location = new System.Drawing.Point(319, 339);
+            this.TBPaciente.Name = "TBPaciente";
+            this.TBPaciente.Size = new System.Drawing.Size(590, 22);
+            this.TBPaciente.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(313, 312);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 24);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Describir sintomas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(313, 226);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 24);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Seleccionar día";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(313, 138);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 24);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Paciente";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label6.Location = new System.Drawing.Point(490, 70);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(242, 24);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Complete todos los campos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(104)))), ((int)(((byte)(91)))));
+            this.label1.Location = new System.Drawing.Point(398, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 58);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Agregar nuevo turno";
+            // 
+            // DTDiaTurno
+            // 
+            this.DTDiaTurno.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DTDiaTurno.Location = new System.Drawing.Point(319, 253);
+            this.DTDiaTurno.MinDate = new System.DateTime(2023, 9, 24, 0, 0, 0, 0);
+            this.DTDiaTurno.Name = "DTDiaTurno";
+            this.DTDiaTurno.Size = new System.Drawing.Size(592, 22);
+            this.DTDiaTurno.TabIndex = 31;
+            // 
+            // CBPaciente
+            // 
+            this.CBPaciente.FormattingEnabled = true;
+            this.CBPaciente.Location = new System.Drawing.Point(317, 165);
+            this.CBPaciente.Name = "CBPaciente";
+            this.CBPaciente.Size = new System.Drawing.Size(590, 24);
+            this.CBPaciente.TabIndex = 32;
+            // 
+            // UC_NuevoTurno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(227)))), ((int)(((byte)(172)))));
+            this.Controls.Add(this.CBPaciente);
+            this.Controls.Add(this.DTDiaTurno);
+            this.Controls.Add(this.BAgregarNuevoTurno);
+            this.Controls.Add(this.TBPaciente);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Name = "UC_NuevoTurno";
+            this.Size = new System.Drawing.Size(1222, 716);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BAgregarNuevoTurno;
+        private System.Windows.Forms.TextBox TBPaciente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker DTDiaTurno;
+        private System.Windows.Forms.ComboBox CBPaciente;
     }
 }
